f1cd3feb2e43ed61ac6061a017dd7f87ffec0dea
nfactorial <nfactorial@btopenworld.com>
2016-03-16 13:34:57 +0000

    Initial commit.

---
+++ development/components/carousel/carousel.jsx
@@ -0,0 +1,16 @@
+"use strict";
+
+import React from 'react';
+import heroData from './welcome_messages.jsx'
+
+
+export default React.createClass({
+    render: function() {
+        return (
+            <header className='hero' id='page-header'>
+                <div className='hero-body dark'>
+                </div>
+            </header>
+        );
+    }
+});
---
+++ development/components/carousel/welcome_messages.jsx
@@ -0,0 +1,29 @@
+"use strict";
+
+// Would make more sense in the future to move this information into a JSON
+// file that is downloaded by the web-page rather than inlining it within the
+// code like this.
+module.exports = {
+    textItems: [
+        {
+            title: 'I\'m nfactorial',
+            small: 'Software Engineer',
+            content: 'Agile, DevOps, Telemetry'
+        },
+        {
+            title: 'I\'m n!',
+            small: 'Video Game Developer',
+            content: 'C++, C#, Java, JavaScript, Python'
+        },
+        {
+            title: 'I\'m nfactorial',
+            small: 'System Designer',
+            content: 'Video Games, Tools, Networking, Rendering'
+        },
+        {
+            title: 'I\'m n!',
+            small: 'Programmer',
+            content: 'Threading, Performance, Cloud'
+        }
+    ]
+};
