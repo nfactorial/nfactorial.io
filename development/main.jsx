426802eed458880dab9242fdf1401d1fe4d43d91
nfactorial <nfactorial@btopenworld.com>
2016-03-16 13:36:40 +0000

    Initial commit.

---
+++ development/main.jsx
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom';
+
+import Application from './application/application.jsx';
+
+ReactDOM.render(
+    <Applicaton/>,
+    document.getElementById('page-root')
+);
---
+++ readme.md
@@ -0,0 +1,2 @@
+This repository contains the source code for a re-write of my
+homepage (http://nfactorial.io) using React.js
